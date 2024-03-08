01cdbf9979a4e70c65ab68127b7e47fde4bec140
Wabene cedric <cedric.wabenemayele.simplon@gmail.com>
2024-03-08 16:35:38 +0100

    add footer

--- index.php
+++ index.php
@@ -1,17 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include './components/fontLinks.php'; ?>
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
     <title>Unity Events</title>
+
+
 </head>
+
 <body>
     <header>
     <?php include './components/mainNavBox.php' ?>
     <?php include './components/volunteerRegisterBox.php' ?>
     </header>
 <?php include './components/background.php' ?>
+    <footer class="footer">
+        <div class="footer-2"> <img src="./assets/logo.png" alt="logo-footer">
+            <div><i class="fa-brands fa-square-facebook"></i>
+            <i class="fa-brands fa-square-twitter"> </i>
+            <i class="fa-brands fa-linkedin"></i>
+            </div>
+        </div>
+        <div class="footer-1">
+            <p>Unite with us to create memorable experiences. Our team of volunteers is dedicated to organizing
+                impactful events that bring people together for a common cause. joi us in marking a throught thr
+                power of community an connection.</p>
+           <p>&copy; 2024 Unity Events</p>
+        </div>
+        <div class="liste">
+            <ul class="liste-2">
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Plan Events</li>
+                <li>Create and Set Up</li>
+                <li>volunteer</li>
+            </ul>
+        </div>
+
+    </footer>
 </body>
+
 </html>
