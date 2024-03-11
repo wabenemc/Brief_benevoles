4acb4f62f96ecfd3e410f9c08f13402836cb5d8c
HaelyosStudio <adam.han.martin@gmail.com>
2024-03-11 13:57:09 +0100

    Main components done +Footer redone

--- pages/registrationStep2.php
+++ pages/registrationStep2.php
@@ -4,24 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include './components/fontLinks.php'; ?>
-    <link rel="stylesheet" href="./css/style.css">
+    <?php include './../components/fontLinks.php'; ?>
+    <link rel="stylesheet" href="./../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <title>Unity Events</title>
+    <title>Volunteer Registration</title>
 
 </head>
 
 <body>
     <header class="headerBox">
-        <?php include './components/mainNavBox.php' ?>
-        <?php include './components/volunteerRegisterBox.php' ?>
-        <?php include './components/background.php' ?>
+        <?php include './../components/mainNavBox.php' ?>
+        <?php include './../components/background.php' ?>
     </header>
     <footer class="footerMainBox">
-    <?php include './components/footer.php' ?>
+    <?php include './../components/footer.php' ?>
     </footer>
 </body>
 
